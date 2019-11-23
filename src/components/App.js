6690a4e5a2542d86ab268e03556ea10b0cb3a13b
tjamesmac <tjamesmac@gmail.com>
2019-11-23 20:34:32 +0000

    Add styles to position content

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,46 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import Navigation from './Navigation.js';
 
-const App = createAppContainer(Navigation);
+const NavigationContainer = createAppContainer(Navigation);
 
+function Head() {
+  return (
+    <View style={styles.header}>
+      <Text>BrewDawg</Text>
+    </View>
+  );
+}
+
+function App() {
+  return (
+    <View style={styles.app}>
+      <Head />
+      <NavigationContainer style={styles.navContainer} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  app: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  header: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 50,
+    paddingBottom: 10,
+    backgroundColor: 'red',
+    fontWeight: 'bold',
+  },
+  headerText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  navContainer: {
+    backgroundColor: 'red',
+  },
+});
 export default App;
