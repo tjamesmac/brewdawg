b190452c0e9ace0356577ca5e2c2a7161a35ad3f
tjamesmac <tjamesmac@gmail.com>
2019-11-24 18:32:21 +0000

    Work on modal

--- src/components/itemScreen/itemScreen.js
+++ src/components/itemScreen/itemScreen.js
@@ -3,9 +3,10 @@ import {
   SafeAreaView,
   StyleSheet,
   View,
+  Modal,
   Text,
+  TouchableHighlight,
   Image,
-  TouchableOpacity,
 } from 'react-native';
 import SwipeGesture from '../swipe/swipeGesture';
 import {
@@ -26,7 +27,15 @@ const BrewScreen: () => React$Node = props => {
   const [data, setData] = React.useState([]);
   const [currentPage, setCurrentPage] = React.useState(8);
   const [APIpage, setAPIpage] = React.useState(1);
-  const [isFetchOpen, setIsFetchOpen] = React.useState(true);
+  const [isModalVisible, setIsModalVisible] = React.useState(false);
+  // const initialModalState = {
+  //   name: 'null',
+  //   tag: 'null',
+  //   abv: 'null',
+  //   description: 'null',
+  //   food: 'null',
+  // };
+  const [modalData, setModalData] = React.useState(null);
 
   function onSwipePerformed(action) {
     switch (action) {
@@ -48,7 +57,7 @@ const BrewScreen: () => React$Node = props => {
         break;
       }
       default: {
-        console.log('Undeteceted action');
+        console.log('Undetected action');
       }
     }
   }
@@ -75,23 +84,87 @@ const BrewScreen: () => React$Node = props => {
   React.useEffect(() => {
     getBeers(1);
   }, []);
-
+  function handleClick(id) {
+    const targetItem = data[id - 1];
+    setModalData(targetItem);
+    setIsModalVisible(true);
+    console.log('click');
+  }
   let getItems;
   if (data.length) {
     const renderRequirements = {
       currentPage,
       data,
       styles,
+      handleClick,
     };
     getItems = renderItems(renderRequirements);
   }
-
+  const CustomModal = props => {
+    const {
+      name,
+      tagline,
+      abv,
+      description,
+      food_pairing,
+      image_url,
+    } = props.itemInformation;
+    console.log(image_url);
+    let imageURL = image_url;
+    const placeHolder = 'https://images.punkapi.com/v2/keg.png';
+    if (image_url === null) {
+      imageURL = placeHolder;
+    }
+    console.log(imageURL, 'null check');
+    return (
+      // <Modal
+      //   animationType="slide"
+      //   transparent={true}
+      //   visible={true}
+      //   onRequestClose={() => {
+      //     // Alert.alert('Modal has been closed.');
+      //   }}>
+      <View style={styles.modalContainer}>
+        <View style={styles.modalBody}>
+          <TouchableHighlight
+            style={styles.modalClose}
+            onPress={() => {
+              setIsModalVisible(!isModalVisible);
+            }}>
+            <Text style={styles.modalCloseText}>CLOSE</Text>
+          </TouchableHighlight>
+          <View styles={styles.itemBody}>
+            <View styles={styles.itemInfo}>
+              {/* <Text style={styles.itemName}>{name}</Text>
+              <Text>{tagline}</Text>
+              <Text>{abv}</Text>
+              <Text>{description}</Text>
+              <Text>{food_pairing}</Text> */}
+            </View>
+            <View styles={styles.itemImageContainer}>
+              <Text>was</Text>
+            </View>
+            <View style={styles.itemImageContainer}>
+              {/* <Image style={styles.itemImage} source={{uri: imageURL}} /> */}
+            </View>
+          </View>
+        </View>
+      </View>
+      // </Modal>
+    );
+  };
+  let renderModal;
+  if (modalData) {
+    renderModal = <CustomModal itemInformation={modalData} />;
+  }
   return (
     <>
       <SafeAreaView>
         <View
           contentInsetAdjustmentBehavior="automatic"
           style={styles.container}>
+          {renderModal}
+          {/* <CustomModal itemInformation={obj} /> */}
           <SwipeGesture
             gestureStyle={styles.swipesGestureContainer}
             onSwipePerformed={onSwipePerformed}>
@@ -108,6 +181,60 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     height: '100%',
   },
+  modalContainer: {
+    marginTop: 100,
+    backgroundColor: 'rgba(100,100,100, 0.5)',
+    // backgroundColor: 'grey',
+    flex: 1,
+    alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  modalBody: {
+    position: 'relative',
+    width: 250,
+    height: 200,
+    backgroundColor: '#333',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+  },
+  modalClose: {
+    position: 'absolute',
+    top: -45,
+    left: 0,
+    backgroundColor: '#e60000',
+    borderRadius: 20,
+    padding: 10,
+  },
+  modalCloseText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  itemBody: {
+    flex: 1,
+    justifyContent: 'center',
+    // borderWidth: 1,
+    // borderColor: 'red',
+    flexDirection: 'row',
+  },
+  itemInfo: {
+    borderWidth: 1,
+    borderColor: 'red',
+    flex: 1,
+    width: 50,
+    height: 50,
+  },
+  itemImageContainer: {
+    borderWidth: 1,
+    borderColor: 'black',
+    width: 125,
+    height: 125,
+  },
+  itemImage: {
+    width: 125,
+    height: 125,
+    resizeMode: 'contain',
+  },
   swipesGestureContainer: {
     // flex: 1,
     // width: '100%',
@@ -121,7 +248,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'wrap',
     width: '100%',
-    // marginTop: 0,
+    marginTop: 450,
     justifyContent: 'space-between',
     paddingLeft: 15,
     paddingRight: 15,
