79a2c17135d4d795c707dcd1e569aa984493e5b9
tjamesmac <tjamesmac@gmail.com>
2019-11-25 16:47:12 +0000

    Fix content

--- src/components/searchScreen/searchScreen.js
+++ src/components/searchScreen/searchScreen.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-
 const SearchScreen: () => React$Node = () => {
   return (
     <>
       <SafeAreaView>
-        <View
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
+        <View style={styles.container}>
           <Text>Search Screen!</Text>
         </View>
       </SafeAreaView>
@@ -18,45 +14,10 @@ const SearchScreen: () => React$Node = () => {
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  swipesGestureContainer: {
+  container: {
     height: '100%',
-    width: '100%',
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
